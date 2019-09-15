a92fb1b909c220ba54c2468313bd91b95b888edc
Matthew C <m23.cullen@gmail.com>
2019-09-15 19:51:24 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,7 +12,9 @@ end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
   if text.match(/\A[A-Z]) != nil && text.match(/\W$/) != nil
-    
+    true
+  else
+    false
 end
 
 def valid_phone_number?(phone)
